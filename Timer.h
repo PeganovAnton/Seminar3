fbe55397347a01a9e6bf5d517615c70be6c121fb
Olga Arkhangelskaia <krishtal.olja@gmail.com>
2020-09-16 23:34:28 +0300

    TravisCI

--- Timer.h
+++ Timer.h
@@ -5,8 +5,8 @@
 #ifndef TTIMER_TIMER_H
 #define TTIMER_TIMER_H
 /*
- * Useful Links
- * https://habr.com/ru/post/324984/
+ * Класс трекера времени. Я назвала его таймер.
+ * Нужен для того чтобы засекать время.
  * */
 
 #include <chrono>
@@ -16,18 +16,41 @@
 
 using namespace std;
 using namespace std::chrono;
+
 class Timer {
 private:
+    /* Сразу вопрос к вам - что такое private: и public: */
+
+    /* Начало отсчета */
     time_point<steady_clock> start;
+    /* Конец отсчета */
     time_point<steady_clock> end;
+    /* Время которое прошло, кстати вопрос - что за тип milliseconds ms */
     milliseconds ms = std::chrono::milliseconds(0);
 public:
     Timer();
+
+    /* Начинаем отсчет времени */
     void start_timer();
+
+    /* Закончикли отсчет времени */
     void stop_timer();
+
+    /* Очистить сохраненное время*/
     void clear_timer();
+
+    /* Вывести время которое мы засекли */
     void print_time();
+
+    /* Вернуть время которое мы засекли в заданном формате
+     * char fmt = s seconds
+     * char fmt = m minutes
+     * char fmt = h hours
+     * */
+
     int get_elapsed_time(char fmt);
+
+    /* Вернуть строку вида h:m:s */
     string format_elapsed_time();
 };
 
