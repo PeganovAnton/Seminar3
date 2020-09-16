a766765e446290db9395562beb0a2928efa2bfbb
Olga Arkhangelskaia <krishtal.olja@gmail.com>
2020-09-16 23:07:55 +0300

    Timer: add main test
    Added funtions in main to test general functionality.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,28 @@
 #include <fstream>
 
 int main() {
+    cout <<"--Timer checks--"<<endl;
+
+    class Timer mtimer;
+    mtimer.start_timer();
+    std::this_thread::sleep_for(1s);
+    mtimer.stop_timer();
+    mtimer.print_time();
+    string rime = mtimer.format_elapsed_time();
+
+    mtimer.start_timer();
+    std::this_thread::sleep_for(2s);
+    mtimer.stop_timer();
+    mtimer.print_time();
+    rime = mtimer.format_elapsed_time();
+    mtimer.start_timer();
+    std::this_thread::sleep_for(3s);
+    mtimer.stop_timer();
+    mtimer.print_time();
+    rime = mtimer.format_elapsed_time();
+    mtimer.clear_timer();
+    mtimer.print_time();
+    cout <<"--Timer checks End--" <<endl<<endl;
     cout <<"Hello World" <<endl;
     return 0;
 }
