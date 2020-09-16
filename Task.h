c13908dfebace3a5bdb0bfed063d0f8d0d107c0f
Olga Arkhangelskaia <krishtal.olja@gmail.com>
2020-09-16 22:41:56 +0300

    Timer: add comments to header

--- Task.h
+++ Task.h
@@ -2,23 +2,55 @@
 // Created by Olga on 16/09/2020.
 //
 
-#ifndef TTIMER_TASK_H
-#define TTIMER_TASK_H
-#include "Timer.h"
+#ifndef TTIMER_TIMER_H
+#define TTIMER_TIMER_H
+/*
+ * Класс трекера времени. Я назвала его таймер.
+ * Нужен для того чтобы засекать время.
+ * */
 
-class Task {
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+using namespace std::chrono;
+class Timer {
 private:
-    class Timer *timer;
-public:
-    string name;
-    string time_spent;
-    Task(string tname);
-    void start();
-    void stop();
-    void statistics();
-    int time_spend();
+    /* Сразу вопрос к вам - что такое private: и public: */
 
+    /* Начало отсчета */
+    time_point<steady_clock> start;
+    /* Конец отсчета */
+    time_point<steady_clock> end;
+    /* Время которое прошло, кстати вопрос - что за тип milliseconds ms */
+    milliseconds ms = std::chrono::milliseconds(0);
+public:
+    Timer();
+
+    /* Начинаем отсчет времени */
+    void start_timer();
+
+    /* Закончикли отсчет времени */
+    void stop_timer();
+
+    /* Очистить сохраненное время*/
+    void clear_timer();
+
+    /* Вывести время которое мы засекли */
+    void print_time();
+
+    /* Вернуть время которое мы засекли в заданном формате
+     * char fmt = s seconds
+     * char fmt = m minutes
+     * char fmt = h hours
+     * */
+
+    int get_elapsed_time(char fmt);
+
+    /* Вернуть строку вида h:m:s */
+    string format_elapsed_time();
 };
 
-
-#endif //TTIMER_TASK_H
+#endif //TTIMER_TIMER_H
