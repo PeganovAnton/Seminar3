4077eff2a98df100db809a1a57565440e27ac2bd
Olga Arkhangelskaia <krishtal.olja@gmail.com>
2020-09-17 10:32:13 +0300

    TimerTask : add Task class

---
+++ Task.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Olga on 16/09/2020.
+//
+
+#include "Task.h"
+Task::Task(string tname) {}
+void Task::start(){};
+void Task::stop(){};
+void Task::statistics() {};
+int Task::time_spend(){return 0;};
+
