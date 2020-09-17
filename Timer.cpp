f0d237b4050b12dcbab6d678d0671e5fe846460d
Kipjatkov Evgenij <kipiatkov.ev@phystech.edu>
2020-09-17 12:09:53 +0300

    Timer: basically implement
    Dumb impementation of format_elapsed_time in Timer.cpp: without controlling
    digits number.

--- Timer.cpp
+++ Timer.cpp
@@ -2,16 +2,62 @@
 // Created by Olga on 16/09/2020.
 //
 
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <sstream>
 #include "Timer.h"
+
+using std::chrono::steady_clock;
+using std::chrono::milliseconds;
+using std::chrono::seconds;
+using std::chrono::minutes;
+using std::chrono::hours;
+using std::chrono::duration_cast;
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::stringstream;
+using std::to_string;
+
 Timer ::Timer() {}
-void Timer::start_timer() {};
+void Timer::start_timer() {
+	start = steady_clock::now();
+};
 
-void Timer::stop_timer(){};
+void Timer::stop_timer() {
+	end = steady_clock::now();
+	ms += duration_cast<milliseconds>(end - start);
+};
 
-void Timer::clear_timer(){};
+void Timer::clear_timer() {
+	ms = milliseconds(0);
+};
 
-void Timer::print_time() {};
+void Timer::print_time() {
+	cout << ms.count() << endl;
+};
 
-int Timer::get_elapsed_time(char fmt) { return 0;};
+int Timer::get_elapsed_time(char fmt) {
+	switch (fmt) {
+	case 's':
+		return duration_cast<seconds>(ms).count();
+	case 'm':
+		return duration_cast<minutes>(ms).count();
+	case 'h':
+		return duration_cast<hours>(ms).count();
+	default:
+		return -1;
+	}
+};
 
-string Timer::format_elapsed_time() { return "";};
+string Timer::format_elapsed_time() { 
+	string result;
+	stringstream str;
+	str << to_string(get_elapsed_time('h')) << ":";
+	str << to_string(get_elapsed_time('m')) << ":";
+	str << to_string(get_elapsed_time('s')) << endl;
+	str >> result;
+	return result;
+};
